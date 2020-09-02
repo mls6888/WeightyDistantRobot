db6249e40bdb3aae8cf39b8e393b072ab26c7360
mls6888 <mls6888@psu.edu>
2020-09-02 17:42:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Author: Liam Sullivan mls6888@psu.edu
+#Collaborator: Olivia Mandola ovm5126@psu.edu
+#Collaborator: Tyler Holman   trh5481@psu.edu
+
+temp = float(input("Enter tempurature: "))
+letter = (input("Enter unit in F/f or C/c: "))
+if letter == "C" or letter == "c":
+  fah
