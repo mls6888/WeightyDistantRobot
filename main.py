8088fe01ca0d682f4cb7fee9afe4a26076c59057
mls6888 <mls6888@psu.edu>
2020-09-02 20:41:13 +0000

    j

--- main.py
+++ main.py
@@ -5,4 +5,11 @@
 temp = float(input("Enter tempurature: "))
 letter = (input("Enter unit in F/f or C/c: "))
 if letter == "C" or letter == "c":
-  fah
+  fah = ((temp * 1.8) + 32)
+  print(f"{temp}° in Celsius is equivalent to {fah}° Fahrenheit.")
+elif letter == "F" or letter == "f":
+  cel = ((temp - 32) /1.8)
+  print(f"{temp}° in Fahrenheit is equivalent to {cel}° Celsius.")
+else:
+  print(f"Invalid unit({letter}).")
+ 
